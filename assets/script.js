9155e04ce6c42bac76935664b160f3ff3010b03e
taylor.martinez <taylor.swot.2018@gmail.com>
2023-09-14 15:34:34 -0500

    created weather functions

--- assets/script.js
+++ assets/script.js
@@ -1,67 +1,115 @@
 // created global variable to be accessed later
 const searchBtn = document.getElementById("search-btn");
 const weatherOptions = document.querySelector(".dropdown");
-var userInputLat = document.getElementById("userInputLat");
-var userInputLon = document.getElementById("userInputLon");
-const currentWeather = document.getElementById("option2");
-const futureForecast = document.getElementById("option3");
+const inputElement = document.getElementById("userInput");
 var displayData = document.querySelector("#results-container");
+var displayDataFiveDay = document.querySelector("#results-container-fiveday");
+var searchHistory = document.getElementById("#history-container");
+
+let userInput = 'las vegas';
 
 
 
 
 function getCity() {
     console.log("button clicked")
+
+    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&appid=f1c5283143e97d8ae4884e8e89154970"
+    fetch (apiUrl) 
+        .then(function (response){
+            return response.json()
+        }) 
+        .then(function (data){
+            console.log(data)
+            console.log(data[0].lat)
+            console.log(data[0].lon)
+            getWeather(data[0].lat, data[0].lon)
+            getFiveDay(data[0].lat, data[0].lon)
+        })
 }
 
 
 
 
-function getWeather() {
-    console.log("weather option clicked");
-    currentWeather.addEventListener("click", getCurrent);
-    function getCurrent() {
-        console.log("current weather displayed")
-    }
-    futureForecast.addEventListener("click", getFuture);
-    function getFuture() {
-        console.log("future weather displayed")
-    }
+function getWeather(lat, lon) {
+    console.log("weather displayed")
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=f1c5283143e97d8ae4884e8e89154970&units=imperial"
+    fetch(apiUrl)
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (data){
+        console.log(data)
+        console.log(data.main.temp)
+        var temp = document.createElement("p")
+        temp.textContent = "temp" + data.main.temp
+        var humidity = document.createElement("p")
+        humidity.textContent = "humidity" + data.main.humidity
+        var windSpeed = document.createElement("p")
+        windSpeed.textContent = "wind: " + data.wind.speed
+        displayData.append(temp, humidity, windSpeed)
+    })
+
+
+
+    // saveSearch()
+
 }
 
-// function getCurrent() {
-//     console.log("current weather displayed")
-// }
+function getFiveDay(lat, lon) {
+    console.log("weather displayed")
 
-// function getFuture() {
-//     console.log("future forecast displayed")
-// }
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=f1c5283143e97d8ae4884e8e89154970&units=imperial"
+    fetch(apiUrl)
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (data){
+        console.log(data)
+        for (var i = 0; i < data.list.length; i += 8) {
+        console.log(data.list[i].main.temp)
+        var temp = document.createElement("p")
+        temp.textContent = "temp: " + data.list[i].main.temp
+        var humidity = document.createElement("p")
+        humidity.textContent = "humidity: " + data.list[i].main.humidity
+        var windSpeed = document.createElement("p")
+        windSpeed.textContent = "wind: " + data.list[i].wind.speed
+        displayDataFiveDay.append(temp, humidity, windSpeed)
+        }
+    })
 
 
-//     // API key = "f1c5283143e97d8ae4884e8e89154970"
-//     var apiUrl = "https://api.openweathermap.org/data/3.0/onecall?" + "lat={lat}" + "&lon={lon}" + "&appid=f1c5283143e97d8ae4884e8e89154970"
 
-//     fetch(apiUrl).then(function (response) {
-//         if(response.ok) {
-//             response.json().then(function (data) {
-//                 displayIssues(data);
-//         }
-// }
+    // saveSearch()
+
+}
 
 
 
+function saveSearch() {
+    console.log("city")
+    // var searchHistory = document.createElement("li");
+    // searchHistory.classList = "list";
+}
 
 
 
 
 searchBtn.addEventListener("click", getCity);
-weatherOptions.addEventListener("click", getWeather);
-// currentWeather.addEventListener("click", getCurrent);
-// futureForecast.addEventListener("click", getFuture);
+inputElement.addEventListener("input", function (event) {
+    userInput = event.target.value;
+});
+
 
 
 
 
 
 // API key = "f1c5283143e97d8ae4884e8e89154970"
-// var apiUrl = "https://api.openweathermap.org/data/3.0/onecall?" + "lat={lat}" + "&lon={lon}" + "&appid=f1c5283143e97d8ae4884e8e89154970"
+
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+// var apiUrl = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid=f1c5283143e97d8ae4884e8e89154970"
+
+
